3f4597b1260daf33c47c1d68c7ca7d3fd7a883a9
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-01-11 00:41:22 +0300

    project completed

---
+++ components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+function Footer() {
+  return <footer className={styles.footer}>Movie App</footer>;
+}
+
+export { Footer };
